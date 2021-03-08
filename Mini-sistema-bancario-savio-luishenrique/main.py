4ef3a68b83f02009bda3ca68f38933c5e0aa8005
henrique15775 <61325756+henrique15775@users.noreply.github.com>
2021-03-08 10:40:17 -0300

    Update main.py

--- Mini-sistema-bancario-savio-luishenrique/main.py
+++ Mini-sistema-bancario-savio-luishenrique/main.py
@@ -12,41 +12,21 @@ tabela_clientes = []
 senha_admin = 'bomdia'
 
 ademiros = [['Henrique',101010,5000,10000,157],['Savio',202020,10000,20000,147],['Governo',000000,1000000000000,10000000000000,177]]
-#list_Bancos = ['Banco do Brasil','CAIXA','Itaú']
-#banks = []
-#for x in range(3):
-  #k = Banco(list_Bancos[x])
-  #banks[x].append(k)
-#print(banks)
+
 e = []
 for x in ademiros:
- # name = input('Digite o seu nome -> ')
- # cpfe = int(input('Digite o número do seu CPF -> '))
- # sal = float(input('Digite o seu salário -> '))
-  
   client = cliente(x[0],x[1],x[2])
-  #tabela_clientes.append(client)
   e.append(conta(client,x[4],x[3]))
   
-  ##client = cliente(name,cpfe,sal)
-  ##tabela_clientes.append(client)
- # c = float(input('Digite o Saldo da conta -> '))
- # id = int(input('Digite o número do ID'))
- ## e = conta(client,id,c)
- ## bank.contas.append(e)
-  #tabela_contas.append(e)
-  #os.system('clear')
+  
 bank_caixa = Banco('Caixa',e[:],[])
 e = []
 people = [['Alfredo',303030,5000,10000,137],['Josefa',404040,10000,20000,127]]
+
 for x in people:
- # name = input('Digite o seu nome -> ')
- # cpfe = int(input('Digite o número do seu CPF -> '))
- # sal = float(input('Digite o seu salário -> '))
   client = cliente(x[0],x[1],x[2])
-  #tabela_clientes.append(client)
   e.append(conta(client,x[4],x[3]))
-  #banks[2].contas.append(e)
+  
 bank_itau = Banco('Itau',e[:],[])
 
 
@@ -87,10 +67,10 @@ while power == 1:
   print('#'*36 + '\n'*3)
   opcao = input('#1- Fazer Transferência  \n#2- Mostrar Financiamentos \n#3- Mostrar dinhero total do Banco \n#4 Mostrar o total financiado \n#5 Fazer um novo financiamento  \n#6 Sacar ou depositar\n\n-> ')
   if opcao == '1':
-    #conta1 = int(input('Digite o número da conta -> '))
+    
     for k in bank_atual.contas:
         if k.ide == conta1:
-          bank_sec = int(input('\nDigite o número correspondente do Banco\n\n 1 Para CAIXA\n 2 Para o ITAU \n\n ->'))
+          bank_sec = int(input('\nDigite o número correspondente do Banco da conta que irá receber\n\n 1 Para CAIXA\n 2 Para o ITAU \n\n ->'))
           conta2 = int(input('Digite o número da conta que você quer transferir -> '))
           bank_send = banks[bank_sec - 1]
           for verify in bank_send.contas:
@@ -104,19 +84,14 @@ while power == 1:
           break
   
   elif opcao == '2':
-    #cliente1 = int(input('Digite o número do seu CPF -> '))
-    #for k in bank_atual:
+    
     for k in bank_atual.contas:
       if k.cliente.cpf == cliente1:
         for x in  k.cliente.financiamentos:
        	 	print(f'Tipo:{x.imovel.tipo}\nBanco:{x.banco.nome_do_banco}\nCódigo -> {x.imovel.codigo}\nValor:{x.imovel.valor}R$\nCPF:{x.cliente.cpf}\n\n')
          	
       time.sleep(6)
-    #for k in tabela_clientes:
-     #   if k.cpf == cliente1:
-      #    for
-       #   print(k.financiamentos)
-        #  time.sleep(3)
+    
         
   elif opcao == '3':
     for x in banks:
@@ -128,16 +103,14 @@ while power == 1:
         time.sleep(10)
         break
   elif opcao == '4':
-    #cliente1 = int(input('Digite o número do seu CPF'))
+  
     for k in bank_atual.contas:
       if k.cliente.cpf == cliente1:
         print(f'{k.cliente.total_financiado()}R$')
         print(f'Seu salario -> {k.cliente.salario}R$')
     input()
   elif opcao == '5':
-   # new_financiamento
-   
-    #cliente1 = int(input('Digite o número do seu CPF -> '))
+
       for k in bank_atual.contas:
         if k.cliente.cpf == cliente1:
           tipo = input('Digite o tipo de imovel -> ')
@@ -146,8 +119,7 @@ while power == 1:
           imov = imovel(tipo,valor,cod)
           value_finan = int(input('Digite o valor do financiamento -> '))
           fin = financiamento(k.cliente,imov,bank_atual,value_finan,0)
-    #k.financiamentos.append(fin)
-    #for p in bank_atual.contas:
+    
           k.cliente.new_financiamento(fin,k)
           break
       time.sleep(2)
@@ -157,10 +129,7 @@ while power == 1:
         opcao = input('Você quer sacar(1) ou depositar(2) ? -> ')
         value = float(input(f'\n\nVocê optou em {opcao}. Digite o valor -> '))
         k.sacar_depositar(value,opcao)
-    #vender
-    #cliente1 = int(input('Digite o número do seu CPF -> '))
-    #bank_choice = int(input('DIgite o banco 1 - Caixa 2 - ITAU'))
-    #bank_create = banks[bank_choice - 1 ]
+    
     
             
   elif opcao == '7':
